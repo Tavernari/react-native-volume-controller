bbe84c49e72f542cbd6c96f88ed314ba0b93d8a3
Victor C Tavernari <victortavernari@gmail.com>
2017-04-01 14:25:38 -0300

    added xcode project to link in react native

--- ios/ReactNativeVolumeController.h
+++ ios/ReactNativeVolumeController.h
@@ -1,4 +1,11 @@
 #import "RCTBridgeModule.h"
+#import <UIKit/UIKit.h>
+
+//! Project version number for ReactNativeVolumeController.
+FOUNDATION_EXPORT double ReactNativeVolumeControllerVersionNumber;
+
+//! Project version string for ReactNativeVolumeController.
+FOUNDATION_EXPORT const unsigned char ReactNativeVolumeControllerVersionString[];
 
 @interface ReactNativeAudioStreaming : NSObject <RCTBridgeModule>
 
